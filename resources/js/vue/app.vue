b7efd3de12edbd596a32ee03b284f46f7d689388
kadek <kadek@gotomalls.com>
2021-11-26 14:51:00 +0800

    show todo list and add new todo

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,11 +1,60 @@
 <template>
     <div class="todoListContainer">
-        hello
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form></add-item-form>
+        </div>
+        <list-view :items="items"></list-view>
     </div>
 </template>
 
 <script>
-export default {
+import addItemForm from "./addItemForm"
+import listItem from "./listItem"
+import listView from "./listView"
 
+export default {
+    components: {
+        addItemForm,
+        listItem,
+        listView
+    },
+    data: function () {
+        return {
+            items: []
+        }
+    },
+    methods: {
+        getList () {
+            axios.get('api/items')
+            .then( response => {
+                this.items = response.data
+            })
+            .catch( error => {
+                console.log(error);
+            })
+        }
+    },
+    created () {
+        this.getList();
+    }
 }
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 350px;
+    margin: auto;
+}
+
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+
+
+</style>
