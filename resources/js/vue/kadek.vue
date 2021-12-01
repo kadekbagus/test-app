3965f77b6ed3c94933c6a0fea6209e60473d7c93
kadek <kadek@gotomalls.com>
2021-12-01 22:47:05 +0800

    fetching data from public api

--- resources/js/vue/kadek.vue
+++ resources/js/vue/kadek.vue
@@ -1,10 +1,30 @@
 <template>
-    <p>hello kadek</p>
+    <div>
+        <p>hello kadek</p>
+        <button v-on:click="getList()">get list user</button>
+        <p v-for="data in listData" :key="data.id"> NAME: {{ data.name.title }} {{ data.name.first }} {{ data.name.last}} </p>
 </template>
 
 
 <script>
 export default {
-    
+    data() {
+        return {
+            listData: []
+        }
+    },
+    created: function() {
+        this.getList()
+    },
+    methods: {
+        async getList() {
+            console.log('hello')
+            const res = await fetch('https://randomuser.me/api?results=10')
+            const re = await res.json()
+            //console.log(re)
+            this.listData = re.results;
+
+        }
+    }
 }
 </script>
